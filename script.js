9106b48d63bfbdc7759909c87d01db2df924e629
Elton Lac <lacelton@yahoo.com>
2023-08-23 13:33:37 -0700

    Hover now colors or erases the squares

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+let color = "black"
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
 
 populateBoard(16);
+
+function changeSize(input) {
+  if (input > 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Too many squares");
+  }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+  color = choice
+}
