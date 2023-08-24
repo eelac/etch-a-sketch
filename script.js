57631946c83145e8a96f884d913ba5d6e3700571
Elton Lac <lacelton@yahoo.com>
2023-08-24 04:39:41 -0700

    Grid updates with size and has borders

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let board = document.querySelector(".grid");
-board.style.gridTemplateRows = "repeat(12, 1fr)";
-board.style.gridTemplateColumns = "repeat(12, 1fr)";
+const gridLayout = (size) => {
+  const container = document.querySelector(".grid-container");
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.setAttribute("class", "grid");
+    container.insertAdjacentElement("beforeend", gridElement);
+  }
+};
+
+gridLayout(12);
