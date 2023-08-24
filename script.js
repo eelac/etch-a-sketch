2e7c40aa16c1afb8beb1c974c27c6f4f998bf880
Elton Lac <lacelton@yahoo.com>
2023-08-24 14:09:15 -0700

    Grid now updates size based on user input

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+var number = document.querySelector(".number");
+
+// Grabs the user's desired number of squares
+const gridSize = () => {
+  var input = number.value;
+  gridLayout(input);
+};
+
 const gridLayout = (size) => {
+  // Defines number of squares
   const container = document.querySelector(".grid-container");
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+  // Clears the board
+  let board = document.querySelector(".grid-container");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+
+  // Populates board with user input number
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
     gridElement.setAttribute("class", "grid");
@@ -10,4 +25,13 @@ const gridLayout = (size) => {
   }
 };
 
-gridLayout(12);
+// Update grid when user leaves the input box or hits enter
+number.addEventListener("focusout", gridSize);
+number.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    gridSize();
+  }
+});
+
+// Default size of the grid
+gridLayout(16);
